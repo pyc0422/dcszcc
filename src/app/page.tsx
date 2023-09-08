718d81d07c269851afa527b938aa083d6b1c79f3
Rain <pyc0422@gmail.com>
2023-09-08 11:31:08 -0500

    modify layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-
+import Footer from '@/components/frames/Footer';
+import Header from '@/components/frames/Header';
 import Container from '@mui/material/Container';
 
 export default function Dashboard() {
   return (
     <Container>
+      <Header />
       <div>main page</div>
+      <Footer/>
     </Container>
   )
 }
