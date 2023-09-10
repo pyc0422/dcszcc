b46ad2aa8bf173ba41446cba0b16cbc99147e2eb
Rain <pyc0422@gmail.com>
2023-09-10 00:05:00 -0500

    modified api route

--- src/app/api/news/[news_id]/route.ts
+++ src/app/api/news/[news_id]/route.ts
@@ -2,6 +2,21 @@ import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import firestore from '../../../../../firebase';
 import { NextResponse } from "next/server";
 export async function GET (request: Request,  { params }: { params: { news_id: string } }) {
-  const news_id = params.news_id;
-  //get specific data from database
+  //return all news data from database
+  if (params) {
+    const news_id = params.news_id;
+  }
+  try {
+    const q = await getDocs(collection(firestore, "news"));
+    const data = q.docs.map((doc) => ({id: doc.id, ...doc.data()}));
+    console.log('data in handler', data)
+    return NextResponse.json({data})
+  } catch(error) {
+    return NextResponse.json(error)
+  }
+}
+
+export async function POST (request:Request) {
+  // for admin endpoint
+  return NextResponse.json('post is not completed')
 }
