27cb1a99bc765e39b5a27b73cd0758e584fad6e1
Rain <pyc0422@gmail.com>
2023-10-09 14:17:15 -0500

    add react-hook-form

--- src/utility/types.ts
+++ src/utility/types.ts
@@ -1,1 +1,14 @@
+import { Timestamp } from "firebase/firestore";
+
 export type AlertType =  {status: boolean; severity: 'error' | 'info' | 'success' | 'warning'; message: string};
+
+export type NewsType = {
+  title:string;
+  news_date:string;
+  important:boolean;
+  bref?:string;
+  content:string;
+  notified:boolean;
+  created_time?:Timestamp;
+  last_update_time?:Timestamp;
+}
