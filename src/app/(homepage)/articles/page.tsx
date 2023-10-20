24a63c4ffd411d818e3e7caa703f81fdaeea1039
Rain <pyc0422@gmail.com>
2023-10-20 11:32:29 -0500

    responsive design

--- src/app/(homepage)/articles/page.tsx
+++ src/app/(homepage)/articles/page.tsx
@@ -20,7 +20,7 @@ export default function Page () {
   return (
     <div className="flex flex-col items-center min-h-screen">
     <div className="p-4 m-2 md:m-8 max-w-[960px] max-h-[800px]">
-      <p className="text-3xl font-medium text-center mb-4">新闻与活动列表</p>
+      <p className="text-xl md:text-3xl font-medium text-center mb-4">新闻与活动列表</p>
       <div className="mt-4 w-full">
       {!newsList?<h1 className="text-center">正在加载，请稍等...</h1>:
       !newsList.length ? <h1 className="text-center">暂时没有新闻</h1>
@@ -28,7 +28,7 @@ export default function Page () {
         <ul className="md:pl-5 list-disc">
           {newsList.map((news) =>
           <li key={news.id} className="p-1 m-1">
-            <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row justify-between items-center border-b-2 border-gray-300 md:border-b-0">
               <a className="w-2/3 hover:underline hover:opacity-60" href={`/articles/${news.id}`}>{news.title}</a>
               <div className="w-1/3 text-right">{news.news_date.toString()}</div>
             </div>
